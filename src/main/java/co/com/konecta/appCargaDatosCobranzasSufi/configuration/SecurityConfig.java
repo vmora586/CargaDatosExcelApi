2697741d6179c4031af8f07c4327a51ab2ac5a69
Victor M <victomc.586@gmail.com>
2019-04-16 13:54:52 -0500

    se implementa el servicio para la autenticación y se configura la clase de seguridad de Springboot

--- src/main/java/co/com/konecta/appCargaDatosCobranzasSufi/configuration/SecurityConfig.java
+++
@@ -1,5 +0,0 @@
-package co.com.konecta.appCargaDatosCobranzasSufi.configuration;
-
-public class SecurityConfig  {
-    
-}
--- src/main/java/co/com/konecta/appCargaDatosCobranzasSufi/service/UserService.java
+++ src/main/java/co/com/konecta/appCargaDatosCobranzasSufi/service/UserService.java
@@ -1,15 +1,52 @@
 package co.com.konecta.appCargaDatosCobranzasSufi.service;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import co.com.konecta.appCargaDatosCobranzasSufi.entity.UserRole;
+import co.com.konecta.appCargaDatosCobranzasSufi.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@Service("userService")
 public class UserService implements UserDetailsService {
 
+	@Autowired
+	@Qualifier("userRepository")
+	private UserRepository userRepository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		co.com.konecta.appCargaDatosCobranzasSufi.entity.User user= userRepository.findByUsername(username);
+		List<GrantedAuthority>auths=builAuthorities(user.getRol());
+		return buildUser(user, auths);
 	}
 
+	private User buildUser(co.com.konecta.appCargaDatosCobranzasSufi.entity.User user,List<GrantedAuthority>authorities) {
+		
+		return new User(user.getName(),user.getPassword(),true,true,true,true, authorities);
+	}
+	
+	
+	private List<GrantedAuthority>builAuthorities (Set<UserRole> userRoles){
+		Set<GrantedAuthority>auths=new HashSet<GrantedAuthority>();
+		
+		for(UserRole userRole: userRoles){
+			auths.add(new SimpleGrantedAuthority(userRole.getName()));
+		}
+		
+		return new ArrayList<GrantedAuthority>(auths);
+		
+	}
 }
